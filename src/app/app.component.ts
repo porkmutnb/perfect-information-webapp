52398ed8181ff37e6d3117907ca29436dac707bf
CHERMEW-DESKTOP <kmutnbenetpor@gmail.com>
2024-12-16 19:00:29 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  [x: string]: any;
   title = 'perfect-information-webapp';
+  isTab = true;
+  isButtonVisible: boolean = false; // แสดง/ซ่อนปุ่ม
+
+  onMenuTab(): void {
+    this.isTab = !this.isTab;
+  }
+
+  // ฟังก์ชันเลื่อนกลับไปด้านบน
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // เพิ่มความลื่นไหล
+    });
+  }
+
+  // ติดตามการเลื่อนของผู้ใช้
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || 0;
+    this.isButtonVisible = scrollPosition > 300; // แสดงปุ่มเมื่อเลื่อนเกิน 300px
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'product', component: ProductComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'prefix'}
+];
